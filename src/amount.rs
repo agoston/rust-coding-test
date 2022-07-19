70cc868ae46927b644e3bc4580372a4a9382a35c
Agoston Horvath <horvath.agoston@gmail.com>
2022-07-19 22:37:12 +0200

    added unit tests to `Amount`. refactored code.

--- src/amount.rs
+++ src/amount.rs
@@ -1,8 +1,16 @@
+use std::fmt;
+use std::fmt::Formatter;
 use std::num::ParseIntError;
 use std::str::FromStr;
+use std::ops::{Add, Mul, Sub};
+
 use crate::amount::Error::{Malformed, PrecisionTooHigh};
 
-/// fixed point precision with 4 fraction digits
+/// fixed point precision with 4 fraction digits, to act as monetary type
+/// (the `decimal` crate is much less efficient qua memory and cpu)
+/// NB: only the operators +-* are implemented!
+/// NB: there is no overflow check, but limit is 2^49, well within practical limits
+#[derive(Debug)]
 pub struct Amount {
     amount_fx4: i64,
 }
@@ -13,6 +21,31 @@ impl Amount {
     }
 }
 
+impl Add for Amount {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Amount { amount_fx4: self.amount_fx4 + rhs.amount_fx4 }
+    }
+}
+
+impl Mul for Amount {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Amount { amount_fx4: (self.amount_fx4 * rhs.amount_fx4) / 10000 }
+    }
+}
+
+impl Sub for Amount {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Amount { amount_fx4: self.amount_fx4 - rhs.amount_fx4 }
+    }
+}
+
+#[derive(Debug)]
 pub enum Error {
     NoInput,
     Malformed,
@@ -20,7 +53,7 @@ pub enum Error {
 }
 
 impl From<ParseIntError> for Error {
-    fn from(e: ParseIntError) -> Self {
+    fn from(_: ParseIntError) -> Self {
         Malformed
     }
 }
@@ -48,36 +81,72 @@ impl FromStr for Amount {
     }
 }
 
-impl From<Amount> for String {
-    fn from(input: Amount) -> Self {
-        let whole = input.amount_fx4 / 10000;
-        let fraction = input.amount_fx4 % 10000;
-        format!("{}.{:04}", whole, fraction)
+impl fmt::Display for Amount {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let whole = self.amount_fx4 / 10000;
+        let mut fraction = self.amount_fx4 % 10000;
+
+        if fraction == 0 {
+            write!(f, "{}", whole)
+        } else {
+            let mut width = 4;
+            // get rid of 'ending zeroes'; this is a fraction after all
+            while fraction % 10 == 0 {
+                fraction = fraction / 10;
+                width = width - 1;
+            }
+            write!(f, "{}.{:0width$}", whole, fraction)
+        }
     }
 }
 
-pub struct Transaction {
-    id: u64,
-    client_id: u64,
-    /// fixed point with 4 decimals (=actual_value*10_000)
-    amount_fx4: u64,
-}
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use crate::amount::Amount;
 
-pub struct Client {
-    id: u64,
-    /// fixed point with 4 decimals (=actual_value*10_000)
-    balance_fx4: u64,
-}
+    #[test]
+    fn parse_test() {
+        let balance: Amount = "12.5".parse().unwrap();
+        assert_eq!(balance.amount_fx4, 125000);
+        assert_eq!(balance.to_string(), "12.5");
+    }
 
-impl Transaction {
-    pub fn new(id: u64, client_id: u64, amount_fx4: u64) -> Transaction {
-        Transaction { id, client_id, amount_fx4 }
+    #[test]
+    fn into_test() {
+        let balance: Amount = Amount::from_str("9.05").unwrap();
+        assert_eq!(balance.amount_fx4, 90500);
+        assert_eq!(balance.to_string(), "9.05");
+    }
+
+    #[test]
+    fn new_test() {
+        let balance = Amount::new(24, 4321);
+        assert_eq!(balance.amount_fx4, 244321);
+        assert_eq!(balance.to_string(), "24.4321");
+    }
+
+    #[test]
+    fn add_test() {
+        let balance = Amount::from_str("11.99").unwrap();
+        let transaction = Amount::from_str("9.99").unwrap();
+        let new_balance = balance + transaction;
+        assert_eq!(new_balance.to_string(), "21.98");
+    }
+
+    #[test]
+    fn sub_test() {
+        let balance = Amount::from_str("11.99").unwrap();
+        let transaction = Amount::from_str("9.99").unwrap();
+        let new_balance = balance - transaction;
+        assert_eq!(new_balance.to_string(), "2");
+    }
+
+    #[test]
+    fn mul_test() {
+        let balance = Amount::from_str("11.99").unwrap();
+        let transaction = Amount::from_str("3").unwrap();
+        let new_balance = balance * transaction;
+        assert_eq!(new_balance.to_string(), "35.97");
     }
 }
-
-impl Client {
-    pub fn new(id: u64, balance_fx4: u64) -> Client {
-        Client { id, balance_fx4 }
-    }
-}
-
--- src/engine.rs
+++ src/engine.rs
@@ -1,2 +1,25 @@
-use crate::amount;
+use crate::amount::Amount;
+
+pub struct Transaction {
+    id: u64,
+    client_id: u64,
+    amount: Amount,
+}
+
+pub struct Client {
+    id: u64,
+    balance: Amount,
+}
+
+// impl Transaction {
+//     pub fn new(id: u64, client_id: u64, amount_fx4: u64) -> Transaction {
+//         Transaction { id, client_id, amount_fx4 }
+//     }
+// }
+//
+// impl Client {
+//     pub fn new(id: u64, balance_fx4: u64) -> Client {
+//         Client { id, balance_fx4 }
+//     }
+// }
 
